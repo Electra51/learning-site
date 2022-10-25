35b88e4039bba19dfa7f17bda99b02f1780bfd80
Electra51 <safayetnurelectra@gmail.com>
2022-10-25 11:31:13 +0600

    navbar added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQRA_HI6GWED1NjyPVRWbsR8m-80_adZ0",
+  authDomain: "learning-site-a4e19.firebaseapp.com",
+  projectId: "learning-site-a4e19",
+  storageBucket: "learning-site-a4e19.appspot.com",
+  messagingSenderId: "198687901655",
+  appId: "1:198687901655:web:0d2de6dc1e7e6d2941ad92"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
